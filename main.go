aca41de824ead282966128f56971ebfdce4fc41a
X-Lord <vipdarlin@126.com>
2023-09-26 04:27:51 +0800

    commit html/template ex

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -16,9 +19,16 @@ func main() {
 	// 	  http.HandleFunc("/", homeHandler)
 
 	r := chi.NewRouter()
+
+	// r.Use(middleware.Logger)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/articles", articleHandler)
+
+	r.With(middleware.Logger).Get("/articles/{id}", articleHandler)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "页面不存在", http.StatusNotFound)
 	})
@@ -29,16 +39,83 @@ func main() {
 	}
 }
 
-func faqHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string, data any) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h2>Q: Is there a free version?</h2><h2>A: Yes, We offer a free trial for 30 days on any paid plans.</h2><br><h2>Q: What are your support hours?</h2><br><h2>A: We are support staff	answering emails 24/7, though response times may be a bit slower on weekends.</h2>")
+
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("解析模板错误: %v", err)
+		http.Error(w, "解析模板错误", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, data)
+	if err != nil {
+		log.Printf("执行模板错误: %v", err)
+		http.Error(w, "执行模板错误", http.StatusInternalServerError)
+		return
+	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html;charset=GBK222")
-	fmt.Fprint(w, "<h1>Main Page1</h1>")
+	templatePath := filepath.Join("cmd", "Ex1", "hello.gohtml")
+
+	executeTemplate(w, templatePath, struct {
+		Name string
+		Age  int
+		Mata struct {
+			Visitors int
+		}
+		Bio string
+	}{
+		Name: "李毅",
+		Age:  32,
+		Mata: struct{ Visitors int }{Visitors: 8},
+		Bio:  `<script>alert("你被黑了")</script>`,
+	})
+
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>our contact</h1><a href='https://baidu.com'>contact Us</a><br>")
+	path := filepath.Join("cmd", "Ex1", "contact.gohtml")
+
+	executeTemplate(w, path, struct {
+		ContactUrl string
+	}{
+		ContactUrl: "http://baidu.com",
+	})
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	path := filepath.Join("cmd", "Ex1", "faq.gohtml")
+	executeTemplate(w, path, struct {
+		Version string
+		Days    int
+		Hours   int
+	}{
+		Version: "v1.0",
+		Days:    7,
+		Hours:   24,
+	})
+}
+
+func articleHandler(w http.ResponseWriter, r *http.Request) {
+	path := filepath.Join("cmd", "Ex1", "ex1.gohtml")
+	executeTemplate(w, path, struct {
+		Cint   int
+		Cfloat float64
+		Cslice []int
+		Cmap   map[string]int
+	}{
+		Cint:   999,
+		Cfloat: 12.23891829381928391283981293,
+		Cslice: []int{1, 2, 3, 4, 5},
+		Cmap: map[string]int{
+			"a": 1,
+			"b": 1,
+			"c": 1,
+			"d": 1,
+			"e": 1,
+		},
+	})
 }
