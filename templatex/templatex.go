5b291d639d34e51d3d6f72ce6a41306940d9efcb
X-Lord <vipdarlin@126.com>
2023-09-27 22:39:53 +0800

    新增templatex

---
+++ templatex/templatex.go
@@ -0,0 +1,30 @@
+package templatex
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type TemplateX struct {
+	Tpl string
+}
+
+func (t TemplateX) Parse(w http.ResponseWriter, filepath string) (*template.Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("解析模板错误: %v", err)
+		http.Error(w, "解析模板错误", http.StatusInternalServerError)
+		return nil, err
+	}
+
+	return tpl, nil
+}
+
+func (t TemplateX) Execute(w http.ResponseWriter, tplParsed *template.Template) {
+	err := tplParsed.Execute(w, nil)
+	if err != nil {
+		log.Printf("渲染模板错误: %v", err)
+		http.Error(w, "渲染模板错误", http.StatusInternalServerError)
+	}
+}
